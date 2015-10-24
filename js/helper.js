4e7fbabe277a7763080a94b914345cf0a972bdb8
Ahsan <ahsanshahidcool@gmail.com>
2015-10-24 19:09:59 -0400

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,4 @@
-/*
 
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1></br><hr/>';
 var HTMLheaderRole = '<span id="role">%data%</span>';
 
@@ -70,9 +57,7 @@ $(document).ready(function() {
   });
 });
 
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
-*/
+
 clickLocations = [];
 
 function logClicks(x,y) {
@@ -91,17 +76,10 @@ $(document).click(function(loc) {
 
 
 
-/*
-This is the fun part. Here's where we generate the custom Google Map for the website.
-See the documentation below for more details.
-https://developers.google.com/maps/documentation/javascript/reference
-*/
 var map;    // declares a global map variable
 
 
-/*
-Start here! initializeMap() is called when page is loaded.
-*/
+
 function initializeMap() {
 
   var locations;
@@ -110,17 +88,11 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
-  For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
-  */
+  
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
-  /*
-  locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
-  */
+  
   function locationFinder() {
 
     // initializes an empty array
@@ -171,9 +143,7 @@ function initializeMap() {
       content: name
     });
 
-    // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
     });
 
     // this is where the pin actually gets added to the map.
@@ -231,9 +201,7 @@ function initializeMap() {
 
 }
 
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
+
 
 // Calls the initializeMap() function when the page loads
 window.addEventListener('load', initializeMap);
